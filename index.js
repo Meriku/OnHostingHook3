480bc010153b13ba737ef956cc3bea6ce554d08d
Meriku <m.mirik.v@gmail.com>
2017-12-11 17:24:29 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,26 +1,6 @@
-var path = require('path');
 var request = require('request');
-
-var TOKEN = '506354993:AAFJQBwRiwvhmnMFiGM57ArA5Mnfa06kdAE';
-
-var baseRequest = request.defaults({
-    baseUrl: 'https://api.telegram.org/bot506354993:AAFJQBwRiwvhmnMFiGM57ArA5Mnfa06kdAE/'
+request('http://www.google.com', function (error, response, body) {
+  console.log('error:', error); // Print the error if one occurred
+  console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
+  console.log('body:', body); // Print the HTML for the Google homepage.
 });
-
-var noop = function (err) {
-    if(err) { console.log(err); }
-};
-var callMethod = function (methodName, params, cb) {
-    cb = cb || noop;
-    var req = {uri: methodName, method: 'POST'};
-    if (Object.keys(params).length) {
-        req.formData = params;
-    }
-    baseRequest(req, function (err, response, body) {
-        // console.log(err, body);
-        if (err) {
-            return cb(err);
-        }
-        cb(err, JSON.parse(body));
-    });
-};
